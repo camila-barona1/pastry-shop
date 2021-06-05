ee9b48986ff377c24244c2b0d000d40013dd3969
Maria CB <camilabarona1@gmail.com>
2021-06-05 00:11:25 -0500

    pastry baked

---
+++ routes/main.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.get("/", (req, res, next) => {
+  res.render("home", req.context);
+});
+
+router.get("/blog", (req, res, next) => {
+  res.render("blog", req.context);
+});
+
+router.get("/menu", (req, res, next) => {
+  res.render("blog", req.context);
+});
+
+module.exports = router;
